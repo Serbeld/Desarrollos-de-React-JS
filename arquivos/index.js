56a4722941229785f3b290761553879c5d8fefcf
serbeld <serbeldiaz@gmail.com>
2021-11-28 12:29:08 -0500

    Add functional Search, Completed Task and Detele item

--- arquivos/index.js
+++ arquivos/index.js
@@ -11,69 +11,132 @@ function countOfCompletedTasks(props) {
 
 function ToDoCounter(props) {
     return (
-        <h2 className="to-do-counter">Has completado <span> { countOfCompletedTasks(props) } </span> de <span> {props.toDosCounter.length} </span> tareas pendientes </h2>
+        <h2 className="to-do-counter">Has completado <span> {countOfCompletedTasks(props)} </span> de <span> {props.toDosCounter.length} </span> tareas pendientes </h2>
     );
 }
 
 function ToDoItem(props) {
+
     return (
         <li className="ToDoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+            <span
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={props.onComplete}
+            >
                 √
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
-            <span className="Icon Icon-delete">
+            <span
+                className="Icon Icon-delete"
+                onClick={props.onDelete}
+            >
                 X
             </span>
         </li>
     );
 }
 
-function ToDoList(props){
-    return(
+function ToDoList(props) {
+    return (
         <section>
             <ul>
-                { props.children }
+                {props.children}
             </ul>
         </section>
     );
 }
 
-function ToDoSearch(props) {
+function ToDoSearch({ searchValue, setSearchValue, text }) {
+    const onSearchValueChange = (event) => {
+        // console.log(event.target.value);
+        setSearchValue(event.target.value);
+    };
+
     return (
-        <input className="TodoSearch" placeholder={ props.text } />
+        <input
+            className="TodoSearch"
+            placeholder={text}
+            value={searchValue}
+            onChange={onSearchValueChange}
+        />
     );
 }
 
-function CreateToDoButton(props){
-    return(
-        <button className="CreateTodoButton">+</button>
+
+
+function CreateToDoButton(props) {
+    const onClickButton = (msg) => {
+        alert(msg);
+    };
+
+    return (
+        <button
+            className="CreateTodoButton"
+            onClick={() => onClickButton('Aquí se debería abrir el modal')}
+        >
+            +
+        </button>
     );
 }
 
-const toDos = [
-    { text: 'Programar Flags', completed: true },
+const defaultToDos = [
+    { text: 'Programar Flags Sale Event', completed: true },
+    { text: 'Flags Bancolombia Carrousel', completed: true },
     { text: 'Ajustar estilos del menú de departamentos', completed: false },
     { text: 'Ajustar estilos de la landing principal', completed: false },
+    { text: 'Agregar filtro de precios', completed: true },
 ];
 
 function App() {
+    const [toDos, setToDo] = React.useState(defaultToDos);
+    const [searchValue, setSearchValue] = React.useState('');
+    let searchedToDos = [];
+
+    if (!searchValue.length >= 1) {
+        searchedToDos = toDos;
+    } else {
+        searchedToDos = toDos.filter(todo => {
+            const todoText = todo.text.toLowerCase();
+            const searchText = searchValue.toLowerCase();
+            return todoText.includes(searchText);
+        });
+    }
+
+    const completeTodo = (text) => {
+        const todoIndex = toDos.findIndex(todo => todo.text === text);
+        const newTodos = [...toDos];
+        newTodos[todoIndex].completed = true;
+        setToDo(newTodos);
+    };
+
+    const deleteTodo = (text) => {
+        const todoIndex = toDos.findIndex(todo => todo.text === text);
+        const newTodos = [...toDos];
+        newTodos.splice(todoIndex, 1);
+        setToDo(newTodos);
+    };
+
     return (
         <React.Fragment>
 
             <ToDoCounter toDosCounter={toDos} />
 
-            <ToDoSearch text="Ingresa un texto relacionado a tu busqueda..." />
+            <ToDoSearch
+                text="Ingresa un texto relacionado a tu busqueda..."
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+            />
 
             <ToDoList>
-                {toDos.map(ToDo => (
+                {searchedToDos.map(ToDo => (
                     <ToDoItem
                         key={ToDo.text}
                         text={ToDo.text}
                         completed={ToDo.completed}
-                    />
+                        onComplete={() => completeTodo(ToDo.text)}
+                        onDelete={() => deleteTodo(ToDo.text)} />
                 ))}
             </ToDoList>
 
@@ -81,7 +144,6 @@ function App() {
         </React.Fragment>
     );
 }
-
 
 ReactDOM.render(
     <App />,
