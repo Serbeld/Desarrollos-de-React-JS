ca54386c40ce3867977cbc6669799633a763f830
serbeld <serbeldiaz@gmail.com>
2021-12-11 11:47:00 -0500

    Custom Hook - Local Storage

--- arquivos/index.js
+++ arquivos/index.js
@@ -89,49 +89,57 @@ function CreateToDoButton(props) {
 //     { text: 'Agregar filtro de precios', completed: true },
 // ];
 
-function App() {
+function useLocalStorage(itemName, initialValue) {
+    const localStorageItem = localStorage.getItem(itemName);
+    let parsedItem;
 
-    const localStorageTodos = localStorage.getItem('TODOS_V1');
-
-    let parsedTodos;
-
-    if (!localStorageTodos) {
-        localStorage.setItem('TODOS_V1', JSON.stringify([]));
-        parsedTodos = [];
+    if (!localStorageItem) {
+        localStorage.setItem(itemName, JSON.stringify(initialValue));
+        parsedItem = initialValue;
     } else {
-        parsedTodos = JSON.parse(localStorageTodos);
+        parsedItem = JSON.parse(localStorageItem);
     }
 
-    const [toDos, setToDo] = React.useState(defaultToDos);
+    const [item, setItem] = React.useState(parsedItem);
+
+    const saveItem = (newItem) => {
+        const stringifiedItem = JSON.stringify(newItem);
+        localStorage.setItem(itemName, stringifiedItem);
+        setItem(newItem);
+    };
+
+    return [
+        item,
+        saveItem,
+    ];
+}
+
+function App() {
+    const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
     const [searchValue, setSearchValue] = React.useState('');
-    let searchedToDos = [];
+
+    let searchedTodos = [];
 
     if (!searchValue.length >= 1) {
-        searchedToDos = toDos;
+        searchedTodos = todos;
     } else {
-        searchedToDos = toDos.filter(todo => {
+        searchedTodos = todos.filter(todo => {
             const todoText = todo.text.toLowerCase();
             const searchText = searchValue.toLowerCase();
             return todoText.includes(searchText);
         });
     }
 
-    const saveTodos = (newTodos) => {
-        const stringifiedTodos = JSON.stringify(newTodos);
-        localStorage.setItem('TODOS_V1', stringifiedTodos);
-        setToDo(newTodos);
-    };
-
     const completeTodo = (text) => {
-        const todoIndex = toDos.findIndex(todo => todo.text === text);
-        const newTodos = [...toDos];
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+        const newTodos = [...todos];
         newTodos[todoIndex].completed = true;
         saveTodos(newTodos);
     };
 
     const deleteTodo = (text) => {
-        const todoIndex = toDos.findIndex(todo => todo.text === text);
-        const newTodos = [...toDos];
+        const todoIndex = todos.findIndex(todo => todo.text === text);
+        const newTodos = [...todos];
         newTodos.splice(todoIndex, 1);
         saveTodos(newTodos);
     };
@@ -139,7 +147,7 @@ function App() {
     return (
         <React.Fragment>
 
-            <ToDoCounter toDosCounter={toDos} />
+            <ToDoCounter toDosCounter={todos} />
 
             <ToDoSearch
                 text="Ingresa un texto relacionado a tu busqueda..."
@@ -148,7 +156,7 @@ function App() {
             />
 
             <ToDoList>
-                {searchedToDos.map(ToDo => (
+                {searchedTodos.map(ToDo => (
                     <ToDoItem
                         key={ToDo.text}
                         text={ToDo.text}
--- react-app/react-app/src/App/index.js
+++ react-app/react-app/src/App/index.js
@@ -11,59 +11,67 @@ import { AppUI } from './AppUI';
 //   { text: 'Agregar filtro de precios', completed: true },
 // ];
 
-function App() {
-
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-
-  let parsedTodos;
-
-  if (!localStorageTodos){
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-  }else{
-    parsedTodos = JSON.parse(localStorageTodos);
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
   }
 
-  const [toDos, setToDo] = React.useState(parsedTodos);
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [
+    item,
+    saveItem,
+  ];
+}
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
-  let searchedToDos = [];
+
+  let searchedTodos = [];
 
   if (!searchValue.length >= 1) {
-    searchedToDos = toDos;
+    searchedTodos = todos;
   } else {
-    searchedToDos = toDos.filter(todo => {
+    searchedTodos = todos.filter(todo => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
     });
   }
 
-  const saveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem('TODOS_V1', stringifiedTodos);
-    setToDo(newTodos);
-  };
-
   const completeTodo = (text) => {
-    const todoIndex = toDos.findIndex(todo => todo.text === text);
-    const newTodos = [...toDos];
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
     saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
-    const todoIndex = toDos.findIndex(todo => todo.text === text);
-    const newTodos = [...toDos];
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
     saveTodos(newTodos);
   };
 
   return (
     <AppUI
-      toDos={toDos}                 
+      toDos={todos}                 
       searchValue={searchValue}
       setSearchValue={setSearchValue}
-      searchedToDos={searchedToDos}
+      searchedToDos={searchedTodos}
       completeTodo={completeTodo}
       deleteTodo={deleteTodo}
     />
