7664df1bbec79881ccfffe28a90dfaf53443668d
serbeld <serbeldiaz@gmail.com>
2021-12-11 11:21:14 -0500

    Add Local Storage On arquivos

--- arquivos/index.js
+++ arquivos/index.js
@@ -81,15 +81,27 @@ function CreateToDoButton(props) {
     );
 }
 
-const defaultToDos = [
-    { text: 'Programar Flags Sale Event', completed: true },
-    { text: 'Flags Bancolombia Carrousel', completed: true },
-    { text: 'Ajustar estilos del menú de departamentos', completed: false },
-    { text: 'Ajustar estilos de la landing principal', completed: false },
-    { text: 'Agregar filtro de precios', completed: true },
-];
+// const defaultToDos = [
+//     { text: 'Programar Flags Sale Event', completed: true },
+//     { text: 'Flags Bancolombia Carrousel', completed: true },
+//     { text: 'Ajustar estilos del menú de departamentos', completed: false },
+//     { text: 'Ajustar estilos de la landing principal', completed: false },
+//     { text: 'Agregar filtro de precios', completed: true },
+// ];
 
 function App() {
+
+    const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+    let parsedTodos;
+
+    if (!localStorageTodos) {
+        localStorage.setItem('TODOS_V1', JSON.stringify([]));
+        parsedTodos = [];
+    } else {
+        parsedTodos = JSON.parse(localStorageTodos);
+    }
+
     const [toDos, setToDo] = React.useState(defaultToDos);
     const [searchValue, setSearchValue] = React.useState('');
     let searchedToDos = [];
@@ -104,18 +116,24 @@ function App() {
         });
     }
 
+    const saveTodos = (newTodos) => {
+        const stringifiedTodos = JSON.stringify(newTodos);
+        localStorage.setItem('TODOS_V1', stringifiedTodos);
+        setToDo(newTodos);
+    };
+
     const completeTodo = (text) => {
         const todoIndex = toDos.findIndex(todo => todo.text === text);
         const newTodos = [...toDos];
         newTodos[todoIndex].completed = true;
-        setToDo(newTodos);
+        saveTodos(newTodos);
     };
 
     const deleteTodo = (text) => {
         const todoIndex = toDos.findIndex(todo => todo.text === text);
         const newTodos = [...toDos];
         newTodos.splice(todoIndex, 1);
-        setToDo(newTodos);
+        saveTodos(newTodos);
     };
 
     return (
